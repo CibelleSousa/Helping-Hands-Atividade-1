1360be82c5132fc0ec1ab57d053f00fa3136dcd6
omniberto <ehmcerqueira.cic@uesc.br>
2025-09-15 21:40:17 -0300

    improvement: comentários adicionados ao css. QoL.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from './logo.png';
 function Header() {
   return (
     <header className="app-header">
-        <a href="/" className="logo-link"><div className="Logo"><img src={logo} id="logo-img"/> <span className="logo-text" id='logo-info'>Helping Hands</span></div></a>
+        <a href="/" className="logo-link"><div className="logo"><img src={logo} id="logo-img"/> <span className="logo-text" id='logo-info'>Helping Hands</span></div></a>
         <nav className="main-nav">
             <ul className="header-icons">
                 <li className="header-icon"><a href="/home" title="Início"><FaHome className="icon-header"/></a></li>
