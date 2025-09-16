7a74f3fd205d574e36253893cf2a61427bf305f1
omniberto <ehmcerqueira.cic@uesc.br>
2025-09-15 21:50:08 -0300

    assets: deleção de asset desnecessário. reorganização de localização de assets.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import { FaHome, FaCommentDots, FaUserAlt, FaRegCalendarAlt } from "react-icons/fa";
-import logo from './logo.png';
+
 function Header() {
   return (
     <header className="app-header">
-        <a href="/" className="logo-link"><div className="logo"><img src={logo} id="logo-img"/> <span className="logo-text" id='logo-info'>Helping Hands</span></div></a>
+        <a href="/" className="logo-link"><div className="logo"><img src={'./src/assets/logo.png'} id="logo-img"/> <span className="logo-text" id='logo-info'>Helping Hands</span></div></a>
         <nav className="main-nav">
             <ul className="header-icons">
                 <li className="header-icon"><a href="/home" title="Início"><FaHome className="icon-header"/></a></li>
